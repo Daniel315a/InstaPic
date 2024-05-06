2785f7a6c396a2772a32bbcc2c6a1fc670bc683a
sebastian vanegas garzon <sebastianvanegasgarzon@outlook.com>
2024-05-06 17:02:22 -0500

    cambios exitosos

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ImageViewerComponent } from '../image-viewer/image-viewer.component';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageViewerComponent
+  ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
